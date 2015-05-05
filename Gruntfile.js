001754730f2220898e6a24402bc4600bb3c45ca3
jjaburke91 <jjaburke@hotmail.com>
2015-05-05 19:19:34 +0100

    Gruntfile now doing a lot of work; concating, minifying, auto updating with bower dependcies, cleaning tmp files, merging views.

---
+++ Gruntfile.js
@@ -0,0 +1,106 @@
+/**
+ * A lot of tips on this found here: http://g00glen00b.be/angular-grunt/
+ *
+ * @param grunt
+ */
+
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        /* Info on wiredep found here: https://github.com/stephenplusplus/grunt-wiredep */
+        wiredep: {
+            task: {
+                // Point to the files that should be updated when
+                // you run `grunt wiredep`
+                src: ['index.php'],
+                options: {
+                }
+            }
+        },
+
+        // installs all bower dependencies to /lib
+        bower: {
+            install: {
+                options: {
+                    install: true,
+                    copy: false,
+                    targetDir: './libs',
+                    cleanTargetDir: true
+                }
+            }
+        },
+
+        // Converts all the views to a js file (somehow)
+        html2js: {
+            dist: {
+                src: [ 'angular/views/*.html' ],
+                dest: 'tmp/views.js'
+            }
+        },
+
+        // Concatenates all angular files and views
+        concat: {
+            options: {
+                separator: ';'
+            },
+            dist: {
+                src: [ 'angular/*.js', 'angular/controllers/*.js', 'angular/directives/*.js', 'tmp/*.js' ],
+                dest: 'dist/app.js'
+            }
+        },
+
+        // Minifys the concatenated js file
+        uglify: {
+            dist: {
+                files: {
+                    'dist/app.js': [ 'dist/app.js' ]
+                },
+                options: {
+                    mangle: false
+                }
+            }
+        },
+
+        // cleans the tmp directory
+        clean: {
+            temp: {
+                src: [ 'tmp' ]
+            }
+        },
+
+        watch: {
+            dev: {
+                files: [ 'Gruntfile.js', 'angular/*.js', 'angular/controllers/*.js', 'angular/directives/*.js', '*.html' ],
+                tasks: [ 'html2js:dist', 'concat:dist', 'clean:temp' ],
+                options: {
+                    atBegin: true
+                }
+            },
+            min: {
+                files: [ 'Gruntfile.js', 'angular/*.js', 'angular/controllers/*.js', 'angular/directives/*.js', '*.html' ],
+                tasks: [ 'html2js:dist', 'concat:dist', 'clean:temp', 'uglify:dist' ],
+                options: {
+                    atBegin: true
+                }
+            }
+        }
+
+    });
+
+    // Loads order of tasks
+    grunt.loadNpmTasks('grunt-wiredep');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-html2js');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-bower-task');
+
+    grunt.registerTask('dev', [ 'bower', 'watch:dev' ]);
+    grunt.registerTask('test', [ 'bower' ]);
+    grunt.registerTask('minified', [ 'bower', 'watch:min' ]);
+    grunt.registerTask('package', [ 'bower', 'html2js:dist', 'concat:dist', 'uglify:dist', 'clean:temp' ]);
+
+};
--- index.php
+++ index.php
@@ -2,6 +2,14 @@
 <html>
 
 <head>
+    <!-- bower:js -->
+    <script src="bower_components/jquery/dist/jquery.js"></script>
+    <!-- endbower -->
+
+    <script type="text/javascript" src="dist/app.js" ></script>
+
+    <link rel="stylesheet" type="text/css" href="bower_components/font-awesome/css/font-awesome.css" />
+    <link rel="stylesheet" type="text/css" href="style.css" />
 
 </head>
 
