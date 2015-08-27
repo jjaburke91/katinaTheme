5b7f79a7a45c4c013944f41c4d66859bb3b935ef
jjaburke91 <jjaburke@hotmail.com>
2015-08-27 22:09:30 +0100

    Basic slidshow displayed on project page with some basic and shitey styling.

--- angular/controllers/project-controller.js
+++ angular/controllers/project-controller.js
@@ -3,4 +3,22 @@ jmApp.controller('projectController', ['$scope', 'project', function( $scope, pr
     $scope.project = project;
     console.log(project);
 
+    $scope.images = project.attachments;
+    $scope.imageSliderIndex = 0;
+
+    $scope.nextImage = function() {
+        if ($scope.imageSliderIndex == $scope.images.length-1 )
+            $scope.imageSliderIndex = 0;
+        else
+            $scope.imageSliderIndex++;
+    };
+    $scope.previousImage = function() {
+        if ($scope.imageSliderIndex == 0)
+            $scope.imageSliderIndex = $scope.images.length-1;
+        else
+            $scope.imageSliderIndex--;
+    };
+
+
+
 }]);
