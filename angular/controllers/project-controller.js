d85983f3bdc923a749de7d8758f947e8ac61d0e8
jjaburke91 <jjaburke@hotmail.com>
2015-08-28 14:17:27 +0100

    Remove lightbox shite from project page.

--- angular/controllers/project-controller.js
+++ angular/controllers/project-controller.js
@@ -4,26 +4,4 @@ jmApp.controller('projectController', ['$scope', 'project', function( $scope, pr
     console.log(project);
 
 
-    // TODO: Following iteration through images is UNORDERED. Perhaps consider giving key values to response object.
-    // http://stackoverflow.com/questions/1129216/sort-array-of-objects-by-string-property-value-in-javascript
-    // Opportunity for some fun functional programming here!
-
-    $scope.images = project.attachments;
-    $scope.imageSliderIndex = 0;
-
-    $scope.nextImage = function() {
-        if ($scope.imageSliderIndex == $scope.images.length-1 )
-            $scope.imageSliderIndex = 0;
-        else
-            $scope.imageSliderIndex++;
-    };
-    $scope.previousImage = function() {
-        if ($scope.imageSliderIndex == 0)
-            $scope.imageSliderIndex = $scope.images.length-1;
-        else
-            $scope.imageSliderIndex--;
-    };
-
-
-
 }]);
