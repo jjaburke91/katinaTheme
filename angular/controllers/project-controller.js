1c01e8d35baf11174f47147889f9a10926af34a9
James Burke <jamesb@audiosplitter.fm>
2015-12-29 15:38:54 +0000

    Updated project information styling. Project changers styled.

--- angular/controllers/project-controller.js
+++ angular/controllers/project-controller.js
@@ -4,6 +4,7 @@ jmApp.controller('projectController', ['$scope', '$rootScope', 'project', functi
 
     $scope.project_highlight = "text-highlight-" + Math.floor( (Math.random()*6)+1); // make sure this is the same as highlight-colours available in stylesheet.
 
+    // Sets border trim feature
     setTimeout( function() {
         $rootScope.projectTitleColour = $scope.project_highlight;
         $rootScope.projectTitleWidth = $('#project-page-title').width() || 0;
@@ -11,22 +12,22 @@ jmApp.controller('projectController', ['$scope', '$rootScope', 'project', functi
         $rootScope.$digest();
     }, 1000);
 
-    function detectScrollToMoveProjectArrows() {
-        var projectInformationScrollTop = $('#project-page-information-container').scrollTop() + 16,
-            isFixed = false;
+/*    function detectScrollToMoveProjectArrows() {
+     var projectInformationScrollTop = $('#project-page-information-container').scrollTop() + 16,
+     isFixed = false;
 
-        return function() {
-            if ( $(this).scrollTop() >= projectInformationScrollTop && !isFixed) {
-                $('.project-changer').addClass('fixed-project-changer');
-                isFixed = true;
-            } else if( $(this).scrollTop() <= projectInformationScrollTop && isFixed) {
-                $('.project-changer').removeClass('fixed-project-changer');
-                isFixed = false;
-            }
-        }
-    }
+     return function() {
+     if ( $(this).scrollTop() >= projectInformationScrollTop && !isFixed) {
+     $('.project-changer').addClass('fixed-project-changer');
+     isFixed = true;
+     } else if( $(this).scrollTop() <= projectInformationScrollTop && isFixed) {
+     $('.project-changer').removeClass('fixed-project-changer');
+     isFixed = false;
+     }
+     }
+     }
 
-    $(window).scroll(
-        _.throttle( detectScrollToMoveProjectArrows(), 500)
-    );
+     $(window).scroll(
+     _.throttle( detectScrollToMoveProjectArrows(), 500)
+     );*/
 }]);
--- angular/directives/header/header.js
+++ angular/directives/header/header.js
@@ -3,30 +3,27 @@ jmApp.directive('jmHeader', function() {
     function controller($scope, $rootScope, $state) {
         $scope.template_dir = $rootScope.template_directory;
 
-        $scope.isHomePage = $state.current;
-        console.log($state.current);
-
-        function detectScrollDirection() {
-            var lastScrollTop = $(this).scrollTop(),
-                headerHeight = $('header').outerHeight(),
-                headerIsHidden = false;
-
-            return function() {
-                var currentScrollTop = $(this).scrollTop();
-
-                if (currentScrollTop > headerHeight || headerIsHidden) {
-                    if (lastScrollTop < currentScrollTop && !headerIsHidden) {
-                        $('header').addClass('hide-header');
-                        headerIsHidden = true;
-                    } else if (lastScrollTop > currentScrollTop && headerIsHidden) {
-                        $('header').removeClass('hide-header');
-                        headerIsHidden = false;
-                    }
-                }
-
-                lastScrollTop = currentScrollTop;
-            }
-        }
+        //function detectScrollDirection() {
+        //    var lastScrollTop = $(this).scrollTop(),
+        //        headerHeight = $('header').outerHeight(),
+        //        headerIsHidden = false;
+        //
+        //    return function() {
+        //        var currentScrollTop = $(this).scrollTop();
+        //
+        //        if (currentScrollTop > headerHeight || headerIsHidden) {
+        //            if (lastScrollTop < currentScrollTop && !headerIsHidden) {
+        //                $('header').addClass('hide-header');
+        //                headerIsHidden = true;
+        //            } else if (lastScrollTop > currentScrollTop && headerIsHidden) {
+        //                $('header').removeClass('hide-header');
+        //                headerIsHidden = false;
+        //            }
+        //        }
+        //
+        //        lastScrollTop = currentScrollTop;
+        //    }
+        //}
 
         //$(window).scroll(_.throttle( detectScrollDirection(), 500) );
     }
